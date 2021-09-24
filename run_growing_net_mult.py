e110fa3d22f0dfe28b5a96eead3f57df1205cb3d
Ethan Caballero <ethanvictorcaballero@gmail.com>
2021-09-24 05:47:50 -0400

    Update run_growing_net_mult.py

--- run_growing_net_mult.py
+++ run_growing_net_mult.py
@@ -6,7 +6,8 @@ run_counter = 0
 for h_size in [10,100,1000,10000,100000]:
     for run in range(runs):
         cwd = os.getcwd()
-        command = "python /network/home/ostapeno/dev/multi_head/run_growing_net.py \
+        #command = "python /network/home/ostapeno/dev/multi_head/run_growing_net.py \
+        command = "python run_growing_net.py \
         --h_size %(h_size)s \
                   " % locals()
         print(command)
