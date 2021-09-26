16d2c3d5bcc5c93341ce87fa73922c88d3fcb470
Ethan Caballero <ethanvictorcaballero@gmail.com>
2021-09-26 06:02:20 -0400

    Update run_growing_net_mult.py

--- run_growing_net_mult.py
+++ run_growing_net_mult.py
@@ -1,9 +1,11 @@
 import os
 import time
 
-runs = 3
+#runs = 3
+runs = 1
 run_counter = 0
-for h_size in [10,100,1000,10000,100000,1000000,10000000]:
+#for h_size in [10,100,1000,10000,100000,1000000,10000000]:
+for h_size in [4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072]:
     for run in range(runs):
         cwd = os.getcwd()
         #command = "python /network/home/ostapeno/dev/multi_head/run_growing_net.py \
